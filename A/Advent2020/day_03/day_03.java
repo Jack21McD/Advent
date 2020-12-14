20e24c24b6c386ef3f2061ea2b3b42224d05d8db
Jack McDowell <mrjack21mcd@gmail.com>
2020-12-13 23:27:35 -0800

    created Java project

---
+++ A/Advent2020/day_03/day_03.java
@@ -0,0 +1,11 @@
+
+/**
+ * day_03
+ */
+public class day_03 {
+
+    public static void main(String[] args) {
+        File file = new File("D:/CompSci/CodePersonal/Advent/Advent2020/day_03/day03input.txt");
+        Scanner user = new Scanner(file);
+    }
+}
